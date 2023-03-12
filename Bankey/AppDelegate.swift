a991b6dcc054fdae7d4a40e94ec2c768de6c4c72
VuslatYolcu <ylcvalt@gmail.com>
2023-03-12 18:28:05 +0300

    Add log out

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -14,31 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let loginViewController = LoginViewController()
     let onboardingContrainerViewController = OnboardingContainerViewController()
+    let dummyViewController = DummyViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         
-        // 4- We are assinging as a delegat so that any signals from loginViewController, we can receive that information
+        // 4- We are assinging as a delegate so that any signals from loginViewController, we can receive that information
         loginViewController.delegate = self
         onboardingContrainerViewController.delegate = self
+        dummyViewController.delegate = self
+        
         window?.rootViewController = loginViewController
     
         return true
-    }
-}
-
-// 5- LoginViewController will say, Hey! login is completed, you can do whatever you want.
-extension AppDelegate: LoginViewControllerDelegate {
-    func didLogin() {
-        setRootViewController(onboardingContrainerViewController)
-    }
-}
-
-extension AppDelegate: OnboardingContainerViewControllerDelegate {
-    func didFinishOnboarding() {
-        print("Did finish onboarding")
     }
 }
 
@@ -55,3 +45,24 @@ extension AppDelegate {
         UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil, completion: nil)
     }
 }
+
+// 5- LoginViewController will say, Hey! login is completed, you can do whatever you want.
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        setRootViewController(onboardingContrainerViewController)
+    }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        setRootViewController(dummyViewController, animated: true)
+    }
+}
+
+extension AppDelegate: LogoutDelegate {
+    func didLogout() {
+        setRootViewController(loginViewController, animated: true)
+    }
+}
+
+
