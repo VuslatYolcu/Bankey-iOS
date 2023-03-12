a826fee3459934714581a08a97296ce24022bc78
VuslatYolcu <ylcvalt@gmail.com>
2023-03-12 16:42:36 +0300

    Add protocol delegate pattern for the didLogin information

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -12,16 +12,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    let loginViewController = LoginViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        //window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
-        //window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989.")
+        
+        // 4- We are assinging as a delegat so that any signals from loginViewController, we can receive that information
+        loginViewController.delegate = self
+        window?.rootViewController = loginViewController
     
         return true
     }
-
 }
 
+// 5- LoginViewController will say, Hey! login is completed, you can do whatever you want.
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        print("Did login")
+    }
+}
--- Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -11,10 +11,8 @@ class OnboardingContainerViewController: UIViewController {
 
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-        }
-    }
+    var currentVC: UIViewController
+    let closeButton = UIButton(type: .system)
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
@@ -39,6 +37,12 @@ class OnboardingContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        style()
+        layout()
+    }
+    
+    private func setup() {
         view.backgroundColor = .systemPurple
         
         // When you want to embed a view controller into a view controller, you need to implement those 3 lines of code
@@ -58,6 +62,21 @@ class OnboardingContainerViewController: UIViewController {
         
         pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
         currentVC = pages.first!
+    }
+    
+    private func style() {
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("Close", for: [])
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+        
+        view.addSubview(closeButton)
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
+        ])
     }
 }
 
@@ -91,4 +110,14 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return pages.firstIndex(of: self.currentVC) ?? 0
     }
+}
+
+// MARK: - Actions
+
+extension OnboardingContainerViewController {
+    
+    @objc func closeTapped(_ sender: UIButton) {
+        // TODO
+    }
+    
 }
