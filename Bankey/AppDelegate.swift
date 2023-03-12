8c2dfe9a4892536f9a5c4b629ded86c8ad337ded
VuslatYolcu <ylcvalt@gmail.com>
2023-03-12 21:31:05 +0300

    Add tab bar

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingContrainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -26,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContrainerViewController.delegate = self
         dummyViewController.delegate = self
         
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
     
         return true
     }
