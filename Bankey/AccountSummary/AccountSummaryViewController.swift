b7aca5cd399a7d44bd28eef5e397d46ca7b8e307
VuslatYolcu <ylcvalt@gmail.com>
2023-03-18 15:02:00 +0300

    Refactor the code

--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -137,17 +137,7 @@ extension AccountSummaryViewController {
             case .success(let profile):
                 self.profile = profile
             case .failure(let error):
-                let title: String
-                let message: String
-                switch error {
-                case .serverError:
-                    title = "Server Error"
-                    message = "Ensure you are connected to the internet. Please try again."
-                case .decodingError:
-                    title = "Decoding Error"
-                    message = "We could not process your request. Please try again."
-                }
-                self.showErrorAlert(title: title, message: message)
+                self.displayError(error: error)
             }
             group.leave()
         }
@@ -158,7 +148,7 @@ extension AccountSummaryViewController {
             case .success(let accounts):
                 self.accounts = accounts
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error: error)
             }
             group.leave()
         }
@@ -188,6 +178,20 @@ extension AccountSummaryViewController {
                                          accountName: $0.name,
                                          balance: $0.amount)
         }
+    }
+    
+    private func displayError(error: NetworkError) {
+        let title: String
+        let message: String
+        switch error {
+        case .serverError:
+            title = "Server Error"
+            message = "Ensure you are connected to the internet. Please try again."
+        case .decodingError:
+            title = "Decoding Error"
+            message = "We could not process your request. Please try again."
+        }
+        self.showErrorAlert(title: title, message: message)
     }
 
     private func showErrorAlert(title: String, message: String) {
